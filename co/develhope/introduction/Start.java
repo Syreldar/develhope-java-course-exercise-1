9b4ce70e2b3114a776296ff323a98803150cd47e
Syreldar <enrico.drago3@gmail.com>
2023-07-11 12:29:25 +0200

    Upload exercise.

--- co/develhope/introduction/Start.java
+++ co/develhope/introduction/Start.java
@@ -4,6 +4,8 @@ public class Start
 {
     public static void main(String[] args)
     {
-
+        String studentName = "Enrico Drago";
+        String birthday = "December 22, 1997";
+        System.out.printf("Hello, Develhope Java student %s! Your birthday is %s.%n", studentName, birthday);
     }
 }
